33ceaf67045b0d11444c465a94ab62b9fbc292a7
Srikrishna S Bhat <srikrbha@in.ibm.com>
2019-08-18 17:16:49 +0530

    Template image shape

--- datasets/siam_mask_dataset.py
+++ datasets/siam_mask_dataset.py
@@ -589,7 +589,7 @@ class DataSets(Dataset):
         reverse_template_box = toBBox(reverse_template_image, reverse_template[1])
         search_box = toBBox(search_image, search[1])
         reverse_search_box = toBBox(reverse_search_image, reverse_search[1])
-        print('Template image shape: {}'.format(template_image))
+        print('Template image shape: {}'.format(template_image.shape))
         print('Template box: {}'.format(template_box))
 
         template, _, _ = self.template_aug(template_image, template_box, self.template_size, gray=gray)
